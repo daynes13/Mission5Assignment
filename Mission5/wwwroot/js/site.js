629f2b70f37357e5351ff8eee7d9af30f22595ce
daynes13 <daynes13@SOPCS-1032206>
2025-02-03 10:13:14 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    // Event handler for the Calculate button
+    $('#calculate-button').click(function () {
+        // Get the value of hours and validate it
+        var hours = $('#hours').val();
+        if (hours <= 0 || isNaN(hours)) {
+            $('#hours-error').show();
+            $('#total').val('');
+        } else {
+            $('#hours-error').hide();
+
+            // Get the hourly rate and convert it to a number
+            var rate = parseFloat($('#rate').val());
+
+            // Calculate the total
+            var total = hours * rate;
+
+            // Display the total
+            $('#total').val(total);
+        }
+    });
+});
